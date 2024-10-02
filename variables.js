d98c81583b7d597da5ab6d021a47397f35188400
Ronny <Ronaldmoore30@gmail.com>
2024-10-02 10:10:52 -0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,16 @@
+console.log('js');
+
+// decalare and initalize a variable
+let testVar = 9;
+
+console.log(testVar);
+
+console.log('the value of testVar is', testVar);
+
+testVar = 34567834;
+
+console.log('the value of testVar is', testVar);
+
+testVar = 'some text other text'
+
+console.log('the value of testVar is', testVar);
